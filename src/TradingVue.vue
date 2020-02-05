b26c17280c1e316da5fbb629c51b61aa47672a94
Laur Aliste <laur.aliste@williamhill.com>
2020-02-05 11:35:35 +0100

    ..

--- src/TradingVue.vue
+++ src/TradingVue.vue
@@ -236,9 +236,11 @@ export default {
             this.$refs.chart.$off('range-changed')
             if (dc) {
                 this.$refs.chart.$on('range-changed',
-                    r => dc.range_changed(
-                        r, this.$refs.chart.interval
-                    )
+                    // TODO: test if vue is ok if Promise here is rejected:
+                    r => new Promise((rs, rj) => rj(`rejected for range ${r}:(`))
+                    // r => dc.range_changed(
+                    //     r, this.$refs.chart.interval
+                    // )
                 )
             }
         }
