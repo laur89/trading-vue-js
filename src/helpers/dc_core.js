e71d8673991b792a835534b4b8696dfeda9c632b
Laur Aliste <laur.aliste@williamhill.com>
2020-02-05 11:44:35 +0100

    dc_core: remove check param from range_changed()
    - looks like it's used to re-try data fetch for the just-fetched
      data range; looks like redundant operation, so is removed.

--- src/helpers/dc_core.js
+++ src/helpers/dc_core.js
@@ -68,7 +68,7 @@ export default class DCCore extends DCEvents {
 
     // Range change callback (called by TradingVue)
     // TODO: improve (reliablity + chunk with limited size)
-    async range_changed(range, tf, check = false) {
+    async range_changed(range, tf) {
 
         if (typeof this.loader !== 'function') return
         if (!this.loading) {  // avoid simultaneous fetches
@@ -101,8 +101,6 @@ export default class DCCore extends DCEvents {
                 }
             }
         }
-
-        if (!check) this.last_chunk = [range, tf]
     }
 
     // A new chunk of data is loaded
@@ -120,11 +118,6 @@ export default class DCCore extends DCEvents {
                 }
             } else {
                 // TODO: how to handle unexpected data? throw and force users to clean up?
-            }
-
-            if (this.last_chunk) {
-                this.range_changed(...this.last_chunk, true)
-                this.last_chunk = null
             }
         } finally {
             this.loading = false
